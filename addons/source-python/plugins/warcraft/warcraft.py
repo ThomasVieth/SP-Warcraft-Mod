847dfe47210d347fc0b432bda772925512b2afac
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2016-07-17 21:26:01 +0100

    Implemented a few test events

--- addons/source-python/plugins/warcraft/warcraft.py
+++ addons/source-python/plugins/warcraft/warcraft.py
@@ -5,6 +5,7 @@ from players.entity import Player
 from messages import SayText2
 
 from .config import WARCRAFT_KILL_EXPERIENCE
+from .config import WARCRAFT_ASSIST_EXPERIENCE
 from .database import load_player_data
 from .database import load_hero_data
 from .database import save_player_data
@@ -51,6 +52,47 @@ def _on_death_save_data(event_data):
 ## EXPERIENCE GAIN
 
 @Event('player_death')
-def _on_kill_give_experience(event_data):
+def _on_kill_assist_give_experience(event_data):
+    if event_data['userid'] == event_data['attacker']:
+        return
+
     attacker = players[event_data['attacker']]
+    victim = players[event_data['userid']]
+    assister = None
+    if event_data['assister']:
+        assister = players[event_data['assister']]
+
     attacker.hero.give_experience(WARCRAFT_KILL_EXPERIENCE)
+
+    if assister:
+        assister.hero.give_experience(WARCRAFT_ASSIST_EXPERIENCE)
+        
+
+## CALL EVENTS
+
+@Event('player_death')
+def _on_kill_assist_call_events(event_data):
+    if event_data['userid'] == event_data['attacker']:
+        player = players[event_data['userid']]
+        player.hero.call_events('player_suicide', player=player)
+        return
+
+    attacker = players[event_data['attacker']]
+    victim = players[event_data['userid']]
+    assister = None
+    if event_data['assister']:
+        assister = players[event_data['assister']]
+
+    attacker.hero.call_events('player_kill', player=attacker, victim=victim,
+        assister=assister)
+    victim.hero.call_events('player_death', player=victim, attacker=attacker,
+        assister=assister)
+    if assister:
+        assister.hero.call_events('player_assist', player=assister, attacker=attacker,
+            victim=victim)
+
+@Event('player_spawn')
+def _on_spawn_call_events(event_data):
+    player = players[event_data['userid']]
+
+    player.hero.call_events('player_spawn', player=player)
