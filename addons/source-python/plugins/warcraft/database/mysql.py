1cd3e8e5cdf580da8de6bcd716184cb0e211cb0a
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2017-01-14 12:20:18 +0000

    Added doc-string for MySQL

--- addons/source-python/plugins/warcraft/database/mysql.py
+++ addons/source-python/plugins/warcraft/database/mysql.py
@@ -18,6 +18,15 @@ __all__ = (
 
 class MySQL(SQLite):
 
+    '''
+
+    MySQL database manager. Loads
+    data into <Player> instances, and
+    saves data from <Player> instances
+    to the database.
+
+    '''
+
     def __init__(self, host=MYSQL_ADDRESS, user=MYSQL_LOGIN, pw=MYSQL_PASSWORD, db=MYSQL_DATABASE_NAME):
         self.connection = connect(host=host, user=user, password=pw, db=db)
         self.cursor = self.connection.cursor()
