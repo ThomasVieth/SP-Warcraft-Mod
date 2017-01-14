2cd3475355a762294a3c9bcce5396cb7a645dec9
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2017-01-14 15:30:43 +0000

    Implemented on_take_damage events

--- addons/source-python/plugins/warcraft/warcraft.py
+++ addons/source-python/plugins/warcraft/warcraft.py
@@ -2,9 +2,14 @@
 
 from commands import CommandReturn
 from commands.say import SayCommand
+from entities import TakeDamageInfo
+from entities.hooks import EntityCondition
+from entities.hooks import EntityPreHook
 from events import Event
 from players.entity import Player
 from players.helpers import userid_from_index
+from players.helpers import userid_from_pointer
+from memory import make_object
 from menus import ListMenu
 from menus import ListOption
 from menus import PagedMenu
@@ -200,3 +205,46 @@ def _on_spawn_call_events(event_data):
     player = players[event_data['userid']]
 
     player.hero.call_events('player_spawn', player=player)
+
+@Event('player_hurt')
+def _on_hurt_call_events(event_data):
+    if event_data['userid'] == event_data['attacker'] or event_data['attacker'] == 0:
+        return
+
+    attacker = players[event_data['attacker']]
+    victim = players[event_data['userid']]
+
+    if victim.team == attacker.team:
+        attacker.hero.call_events('player_teammate_attack', player=attacker,
+            victim=victim, attacker=attacker)
+        victim.hero.call_events('player_teammate_victim', player=victim,
+            attacker=attacker, victim=victim)
+        return
+
+    attacker.hero.call_events('player_attack', player=attacker, victim=victim,
+        attacker=attacker)
+    victim.hero.call_events('player_victim', player=victim, attacker=attacker,
+        victim=victim)
+
+@EntityPreHook(EntityCondition.is_player, 'on_take_damage')
+def _pre_damage_call_events(args):
+    take_damage_info = make_object(TakeDamageInfo, args[1])
+    if not take_damage_info.attacker:
+        return
+    attacker = players[userid_from_index(take_damage_info.attacker)]
+    victim = players[userid_from_pointer(args[0])]
+
+    event_args = {
+        'attacker': attacker,
+        'victim': victim,
+        'take_damage_info': take_damage_info,
+    }
+
+    if victim.team == attacker.team:
+        attacker.hero.call_events('player_pre_teammate_attack', player=attacker,
+            **event_args)
+        victim.hero.call_events('player_pre_teammate_victim', player=victim, **event_args)
+        return
+
+    attacker.hero.call_events('player_pre_attack', player=attacker, **event_args)
+    victim.hero.call_events('player_pre_victim', player=victim, **event_args)
