4ceac9eb3c2ec714742d4e818b99941360694d7a
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2017-01-19 08:32:41 +0000

    Created more skills for the Test hero

--- addons/source-python/plugins/warcraft/skills/undead.py
+++ addons/source-python/plugins/warcraft/skills/undead.py
@@ -4,6 +4,8 @@ from ..heroes.undead import Undead
 from . import Skill
 from . import events
 
+from random import randint
+
 @Undead.skill
 class Unholy(Skill):
     name = 'Unholy Aura'
@@ -12,3 +14,31 @@ class Unholy(Skill):
     @events('player_spawn')
     def _on_spawn(self, player):
         player.speed += 0.06 * self.level
+
+@Undead.skill
+class Levitation(Skill):
+    name = 'Levitation'
+    max_level = 8
+
+    @events('player_spawn')
+    def _on_spawn(self, player):
+        player.gravity = 1 - (0.08 * self.level)
+
+    @events('player_death')
+    def _on_death(self, player, **kwargs):
+        player.gravity = 1.0
+
+@Undead.skill
+class Vampiric(Skill):
+    name = 'Vampiric Aura'
+    max_level = 8
+
+    divisor = 1.5
+
+    @events('player_pre_attack')
+    def _on_pre_attack(self, player, take_damage_info, **kwargs):
+        if randint(0, 100) > 35:
+            return
+
+        health_to_leech = round(take_damage_info.damage / self.divisor)
+        player.health += health_to_leech
