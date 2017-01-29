ea9f54479b024b555164da6215c5a1feaee284dc
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2017-01-29 22:13:43 +0000

    Corrected an import for cooldowns

--- addons/source-python/plugins/warcraft/cooldown.py
+++ addons/source-python/plugins/warcraft/cooldown.py
@@ -16,15 +16,15 @@ class Cooldown:
 
     def __init__(self, duration):
         self.max_cooldown = duration
-        self._end_time = time.time() + duration
+        self._end_time = time() + duration
 
         log(2, 'Creating cooldown with length ({} seconds)'.format(duration))
 
     @property
     def is_over(self):
-        return self._end_time <= time.time()
+        return self._end_time <= time()
 
     @property
     def remaining(self):
-        r = time.time() - self._end_time
+        r = time() - self._end_time
         return r if r > 0 else 0
