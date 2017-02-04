5794ca53f25e789834efb7f791ba80a7cb8e4930
Exiistenz <lilian78800@hotmail.fr>
2017-02-04 15:51:40 +0100

    Add xp system on some new events

--- addons/source-python/plugins/warcraft/experience.py
+++ addons/source-python/plugins/warcraft/experience.py
@@ -2,8 +2,7 @@
 
 from events import Event
 
-from .config import WARCRAFT_KILL_EXPERIENCE
-from .config import WARCRAFT_ASSIST_EXPERIENCE
+from .config import *
 from .players import players
 from .strings import give_experience
 
@@ -12,6 +11,13 @@ from .strings import give_experience
 __all__ = tuple()
 
 ## EXPERIENCE GAIN
+
+@Event('player_spawn')
+def _on_spawn_give_experience(event_data):
+    player = players[event_data['userid']]
+    exp = SPAWN_EXPERIENCE * MULTIPLIER_EXPERIENCE
+    player.hero.give_experience(exp);
+    give_experience.send(player.index, amount=exp, reason='on spawn')
 
 @Event('player_death')
 def _on_kill_assist_give_experience(event_data):
@@ -32,3 +38,41 @@ def _on_kill_assist_give_experience(event_data):
         assister.hero.give_experience(WARCRAFT_ASSIST_EXPERIENCE)
         give_experience.send(assister.index, amount=WARCRAFT_ASSIST_EXPERIENCE,
         reason='for assisting a kill')
+        
+@Event('round_mvp')
+def _give_xp_on_mvp(event_data):
+    player = players[event_data['userid']]
+    exp = SPAWN_EXPERIENCE * MULTIPLIER_EXPERIENCE
+    give_experience.send(player.index, amount=exp, reason='for being MVP')
+    player.hero.give_experience(exp)
+
+@Event('round_end')
+def _give_xp_on_round_end(event_data):
+    for player in players.values():
+        if player.team == 1:
+            return
+        key = 'WIN' if player.team == event_data['winner'] else 'LOSS'
+        exp = eval(key + '_EXPERIENCE') * MULTIPLIER_EXPERIENCE
+        give_experience.send(player.index, amount=exp, reason='for winning round')
+        player.hero.give_experience(exp) 
+        
+@Event('bomb_planted')
+def _give_xp_on_plant(event_data):
+    player = players[event_data['userid']]
+    exp = PLANT_EXPERIENCE * MULTIPLIER_EXPERIENCE
+    player.hero.give_experience(exp)
+    give_experience.send(player.index, amount=exp, reason='for planting bomb')
+    
+@Event('bomb_defused')
+def _give_xp_on_defuse(event_data):
+    player = players[event_data['userid']]
+    exp = DEFUSE_EXPERIENCE * MULTIPLIER_EXPERIENCE
+    player.hero.give_experience(exp)
+    give_experience.send(player.index, amount=exp, reason='for planting bomb')
+    
+@Event('hostage_follows')
+def _hostage_follows(event_data):
+    player = players[event_data['userid']]
+    exp = HOSTAGE_PICK_EXPERIENCE * MULTIPLIER_EXPERIENCE
+    player.hero.give_experience(exp)
+    give_experience.send(player.index, amount=exp, reason='for planting bomb')
