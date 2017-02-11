e39117683836eef5b7a704ff2b2468f86a97ccf0
Exiistenz <lilian78800@hotmail.fr>
2017-02-11 16:31:45 +0100

    Show spendskills and effect on level up

--- addons/source-python/plugins/warcraft/warcraft.py
+++ addons/source-python/plugins/warcraft/warcraft.py
@@ -2,13 +2,18 @@
 
 from commands import CommandReturn
 from commands.say import SayCommand
+from engines.precache import Model
+from entities.entity import Entity
+from entities.helpers import index_from_pointer
 from events import Event
+from listeners.tick import Delay
 from players.helpers import userid_from_index
 
 from .calls import *
 from .experience import *
 from .heroes import *
 from .info import *
+from .listeners import HeroLevelChange
 from .menus import *
 from .players import *
 from .strings import *
@@ -51,3 +56,23 @@ def _on_spawn_send_show_xp(event_data):
     show_experience.send(player.index, hero=player.hero.name, level=player.hero.level,
         experience=player.hero.experience,
         needed=player.hero.required_experience(player.hero.level))
+    
+## LEVEL UP
+
+@HeroLevelChange
+def _on_level_change(hero):
+    player = hero.owner
+    spend_skills.send(player)
+    pointer = player.give_named_item('env_smokestack', 0, None, False)
+    entity = Entity(index_from_pointer(pointer))
+
+    Model('effects/yellowflare.vmt')
+    for output in ('basespread 10', 'spreadspeed 60', 'initial 0', 'speed 105',
+        'rate 50', 'startsize 7', 'endsize 2', 'twist 0', 'jetlength 100',
+        'angles 0 0 0', 'rendermode 18', 'renderamt 100',
+        'rendercolor 255 255 3', 'SmokeMaterial effects/yellowflare.vmt'):
+        entity.add_output(output)
+
+    entity.turn_on()
+    entity.set_parent(player.pointer, -1)
+    Delay(0.5, entity.turn_off)
