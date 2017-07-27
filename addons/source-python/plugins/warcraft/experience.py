ae3ec92e78bb6fa28a264d0b2a93e0e3906ee409
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2017-07-27 14:14:06 +0100

    Added more ways to gain experience

--- addons/source-python/plugins/warcraft/experience.py
+++ addons/source-python/plugins/warcraft/experience.py
@@ -2,8 +2,10 @@
 
 from events import Event
 
-from .config import WARCRAFT_KILL_EXPERIENCE
-from .config import WARCRAFT_ASSIST_EXPERIENCE
+from .config import (WARCRAFT_KILL_EXPERIENCE, WARCRAFT_ASSIST_EXPERIENCE,
+    WARCRAFT_ROUND_WIN_EXPERIENCE, WARCRAFT_ROUND_LOSS_EXPERIENCE,
+    WARCRAFT_PLANT_EXPERIENCE, WARCRAFT_EXPLODE_EXPERIENCE, WARCRAFT_DEFUSE_EXPERIENCE,
+    WARCRAFT_CHICKEN_EXPERIENCE)
 from .players import players
 from .strings import give_experience
 
@@ -12,6 +14,16 @@ from .strings import give_experience
 __all__ = tuple()
 
 ## EXPERIENCE GAIN
+
+@Event('other_death')
+def _on_kill_other_give_experience(event_data):
+    if event_data['userid'] == event_data['attacker'] or event_data['attacker'] == 0:
+        return
+
+    attacker = players.from_userid(event_data['attacker'])
+    attacker.hero.give_experience(WARCRAFT_CHICKEN_EXPERIENCE)
+    give_experience.send(attacker.index, amount=WARCRAFT_CHICKEN_EXPERIENCE,
+        reason='for killing a chicken')
 
 @Event('player_death')
 def _on_kill_assist_give_experience(event_data):
@@ -32,3 +44,37 @@ def _on_kill_assist_give_experience(event_data):
         assister.hero.give_experience(WARCRAFT_ASSIST_EXPERIENCE)
         give_experience.send(assister.index, amount=WARCRAFT_ASSIST_EXPERIENCE,
         reason='for assisting a kill')
+
+@Event('round_end')
+def _on_round_end_give_experience(event_data):
+    winner = event_data['winner']
+    for player in players.values():
+        if player.team == winner:
+            player.hero.give_experience(WARCRAFT_ROUND_WIN_EXPERIENCE)
+            give_experience.send(player.index, amount=WARCRAFT_ROUND_WIN_EXPERIENCE,
+                reason='for winning a round')
+        elif player.team == 5-winner:
+            player.hero.give_experience(WARCRAFT_ROUND_LOSS_EXPERIENCE)
+            give_experience.send(player.index, amount=WARCRAFT_ROUND_LOSS_EXPERIENCE,
+                reason='for losing a round')
+
+@Event('bomb_planted')
+def _on_plant_give_experience(event_data):
+    player = players.from_userid(event_data['userid'])
+    player.hero.give_experience(WARCRAFT_PLANT_EXPERIENCE)
+    give_experience.send(player.index, amount=WARCRAFT_PLANT_EXPERIENCE,
+                reason='for planting the bomb')
+
+@Event('bomb_exploded')
+def _on_explode_give_experience(event_data):
+    player = players.from_userid(event_data['userid'])
+    player.hero.give_experience(WARCRAFT_EXPLODE_EXPERIENCE)
+    give_experience.send(player.index, amount=WARCRAFT_EXPLODE_EXPERIENCE,
+                reason='for making the bomb explode')
+
+@Event('bomb_defused')
+def _on_defuse_give_experience(event_data):
+    player = players.from_userid(event_data['userid'])
+    player.hero.give_experience(WARCRAFT_DEFUSE_EXPERIENCE)
+    give_experience.send(player.index, amount=WARCRAFT_DEFUSE_EXPERIENCE,
+                reason='for defusing the bomb')
