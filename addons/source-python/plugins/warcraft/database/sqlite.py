a9d3e66228c7f60271a59886aa62aad7cd576297
Xiazee <Xiazee@users.noreply.github.com>
2017-10-10 18:55:17 +0200

    Fixed sql injection due to string.format()

--- addons/source-python/plugins/warcraft/database/sqlite.py
+++ addons/source-python/plugins/warcraft/database/sqlite.py
@@ -62,19 +62,19 @@ class SQLite:
     # players, and heroes.
 
     def add_player(self, player):
-        self.execute("INSERT INTO Players (SteamID, Hero, Name) VALUES ('{}', '{}', '{}')".format(
-            player.uniqueid, WARCRAFT_DEFAULT_HERO, player.name))
+        self.execute("INSERT INTO Players (SteamID, Hero, Name) VALUES (?, ?, ?)",
+                     (player.uniqueid, WARCRAFT_DEFAULT_HERO, player.name))
         self.add_hero(player, Hero.get_subclass_dict()[WARCRAFT_DEFAULT_HERO]())
 
     def add_hero(self, player, hero):
-        self.execute("INSERT INTO Heroes (SteamID, Name, Experience, Level) VALUES ('{}', '{}', {}, {})".format(
-            player.uniqueid, hero.__class__.__name__, 0, 0))
+        self.execute("INSERT INTO Heroes (SteamID, Name, Experience, Level) VALUES (?, ?, ?, ?)",
+                     (player.uniqueid, hero.__class__.__name__, 0, 0))
         for skill in hero.skills:
             self.add_skill(player, hero, skill)
 
     def add_skill(self, player, hero, skill):
-        self.execute("INSERT INTO Skills (SteamID, Hero, Name, Level) VALUES ('{}', '{}', '{}', {})".format(
-            player.uniqueid, hero.__class__.__name__, skill.__class__.__name__, 0))
+        self.execute("INSERT INTO Skills (SteamID, Hero, Name, Level) VALUES (?, ?, ?, ?)",
+                     (player.uniqueid, hero.__class__.__name__, skill.__class__.__name__, 0))
 
 
     # Retrievable data from tables, please
@@ -82,37 +82,37 @@ class SQLite:
 
 
     def get_player_hero(self, player):
-        self.execute("SELECT Hero FROM Players WHERE SteamID='{}'".format(
-            player.uniqueid, ))
+        self.execute("SELECT Hero FROM Players WHERE SteamID=?",
+                     (player.uniqueid, ))
         fetched_data = self.cursor.fetchone()
         if fetched_data is None:
             return None
         return fetched_data[0]
 
     def get_player_name(self, uniqueid):
-        self.execute("SELECT Name FROM Players WHERE SteamID='{}'".format(
-            uniqueid, ))
+        self.execute("SELECT Name FROM Players WHERE SteamID=?"
+                     (uniqueid, ))
         fetched_data = self.cursor.fetchone()
         if fetched_data is None:
             return None
         return fetched_data[0]
 
     def get_player_hero_data(self, player):
-        self.execute("SELECT Experience, Level FROM Heroes WHERE SteamID='{}' AND Name='{}'".format(
-            player.uniqueid, player.hero.__class__.__name__))
+        self.execute("SELECT Experience, Level FROM Heroes WHERE SteamID=? AND Name=?",
+                     (player.uniqueid, player.hero.__class__.__name__))
         return self.cursor.fetchone()
 
     def get_player_skill_level(self, player, hero, skill):
-        self.execute("SELECT Level FROM Skills WHERE SteamID='{}' AND Hero='{}' AND Name='{}'".format(
-            player.uniqueid, hero.__class__.__name__, skill.__class__.__name__))
+        self.execute("SELECT Level FROM Skills WHERE SteamID=? AND Hero=? AND Name=?",
+                     (player.uniqueid, hero.__class__.__name__, skill.__class__.__name__))
         fetched_data = self.cursor.fetchone()
         if fetched_data is None:
             return None
         return fetched_data[0]
 
     def get_total_level(self, player):
-        self.execute("SELECT SUM(Level) FROM Heroes WHERE SteamID='{}'".format(
-            player.uniqueid))
+        self.execute("SELECT SUM(Level) FROM Heroes WHERE SteamID=?",
+                     (player.uniqueid, ))
         fetched_data = self.cursor.fetchone()
         if fetched_data is None:
             return None
@@ -137,17 +137,17 @@ class SQLite:
 
 
     def set_player_hero(self, player):
-        self.execute("UPDATE Players SET Hero='{}' WHERE SteamID='{}'".format(
-            player.hero.__class__.__name__, player.uniqueid))
+        self.execute("UPDATE Players SET Hero=? WHERE SteamID=?",
+                     (player.hero.__class__.__name__, player.uniqueid))
 
     def set_player_name(self, player):
-        self.execute("UPDATE Players SET Name='{}' WHERE SteamID='{}'".format(
-            player.name, player.uniqueid))
+        self.execute("UPDATE Players SET Name=? WHERE SteamID=?",
+                     (player.name, player.uniqueid))
 
     def set_player_hero_data(self, player, hero):
-        self.execute("UPDATE Heroes SET Experience={}, Level={} WHERE SteamID='{}' AND Name='{}'".format(
-            hero.experience, hero.level, player.uniqueid, hero.__class__.__name__))
+        self.execute("UPDATE Heroes SET Experience=?, Level=? WHERE SteamID=? AND Name=?",
+                     (hero.experience, hero.level, player.uniqueid, hero.__class__.__name__))
 
     def set_player_skill_level(self, player, hero, skill):
-        self.execute("UPDATE Skills SET Level={} WHERE SteamID='{}' AND Hero='{}' AND Name='{}'".format(
-            skill.level, player.uniqueid, hero.__class__.__name__, skill.__class__.__name__))
+        self.execute("UPDATE Skills SET Level=? WHERE SteamID=? AND Hero=? AND Name=?",
+                     (skill.level, player.uniqueid, hero.__class__.__name__, skill.__class__.__name__))
